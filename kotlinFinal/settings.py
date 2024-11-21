ceb28007d3980abb26c80f006e58f0a820cef332
Spiral0 <reubenwachira75@gmail.com>
2024-11-21 14:02:35 +0300

    msql implementation

--- kotlinFinal/settings.py
+++ kotlinFinal/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'kotlinFinal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kotlinfinal',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
